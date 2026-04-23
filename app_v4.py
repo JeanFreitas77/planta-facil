ccb1517b0f3265525302127515094589ae36c441
Jean Freitas <jean.freitas@serverrima.local>
2026-04-23 17:52:01 -0300

    ajuste para usar secrets do streamlit

--- app_v4.py
+++ app_v4.py
@@ -66,8 +66,8 @@ st.markdown("""
 # CONEXÃO SUPABASE
 # =========================
 # IMPORTANTE: Substitua pelas suas credenciais reais
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url = st.secrets.get("SUPABASE_URL") or os.getenv("SUPABASE_URL")
+key = st.secrets.get("SUPABASE_KEY") or os.getenv("SUPABASE_KEY")
 
 @st.cache_resource
 def get_supabase():
